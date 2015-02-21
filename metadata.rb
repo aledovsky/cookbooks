f9c12c2d2e2ce2aee088adfd5ea84e41711008b4
Aleksandrs Ļedovskis <aleksandrs@ledovskis.lv>
2015-02-21 19:38:11 +0200

    Make compatible with Chef 12.0
    - Modify metadata.rb info a bit
    - Increment patch version
    
    Signed-off-by: Aleksandrs Ļedovskis <aleksandrs@ledovskis.lv>

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,7 @@
-maintainer       "Room 118 Solutions, Inc."
-maintainer_email "info@room118solutions.com"
+name             "postfix-dkim"
+maintainer       "Aleksandrs Ļedovskis"
+maintainer_email "aleksandrs@ledovskis.lv"
 license          "Apache 2.0"
 description      "Installs/Configures opendkim, a postfix DKIM filter (see: https://help.ubuntu.com/community/Postfix/DKIM)"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.3"
+version          "0.0.4"
